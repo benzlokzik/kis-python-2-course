00b1665b9696a36ebff7aeeeab2f5ab6cbb3a563
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2024-02-24 18:11:08 +0300

    week3

---
+++ seminars/week3/main.py
@@ -0,0 +1,60 @@
+#create file with all pep8 eroors
+
+#Path: seminars/week3/main.py
+C=1
+a=0
+def f(*,x :float, z: float ,y:float,q, jjjjjj : int)->None:
+      """"""
+      global C
+      ф : int = 1
+      x,y,z= 1,2,3
+      zzzzzzz=lambda x,y,z: x+y+z
+      if type(x) is int: print("x is int")
+      x = x + 1               #Increment x
+      x = x + 1                  #Increment x
+      a= (y - z**3/88)**2
+      I= (x - y**2/28)**6
+      c= 31*x**4 + 77 *( ( z**3 + y**2 + 70*y)**7)
+      d= (((11*x**2)**6)
+          +
+      ((y**3 + z**2)**3))
+      e = (((a + I -
+        (c/d))))
+
+      with (open("pep8_errors.txt", "r")) as file :
+          a =file.readlines( );for i       in      a: print (i ); print (i); print ( "lol yandex use 179 symbols")	if	file.readlines() == 179 else print ("lol yandex use 179 symbols",	2 if None else 4 if True else 0)
+
+	  print('\t')
+      if ((C == 1) and
+ 0!=0):
+            print('\n')
+
+      e = (
+          'a', 'b', 'c',
+          'd', 'e', 'f',
+           )
+      e = [
+    'a', 'b','c',
+         'd', 'e', 'f',
+]
+      f = (e[: : -1] +
+           e[1: : -1])
+      e =(1, )
+      if None : print( 1) ; a , e =e,a
+      return 1
+def IO(x, z, y):
+       return f(x, z, y)
+
+def IO2(x, z, y):
+       """hehe
+"""
+       if not 1 is None: f ()
+       for i in range(10): print(i)
+       while 0: return f (x=1,)
+       return f ()
+class Io:
+    def __init__(self, x, z, y):
+        self.x = x
+        self.z = z
+        self.y = y
+import sys, os
